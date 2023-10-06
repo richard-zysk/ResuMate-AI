c1c453606030d42b5bb666c15be46a28d5dcb7a8
rishikalokesh2512 <77378808+rishikalokesh2512@users.noreply.github.com>
2023-10-06 13:39:01 +0530

    Update table.tsx
    WIP: fetch Resumes Data

--- client/src/components/grid/table.tsx
+++ client/src/components/grid/table.tsx
@@ -13,6 +13,7 @@ import DropzoneComponent from "../common/dropzone";
 import CustomSnackbar from "../common/snackBar";
 import { useAuth } from "../auth/authProvider";
 import { apiget, apipost } from "../../services/axiosClient";
+import { BASE_URL } from "../../appConstants";
 
 const columns: GridColDef[] = [
   { field: "id", headerName: "ID", width: 90 },
@@ -97,66 +98,101 @@ export default function Table() {
     msg: "uploaded!!",
     color: "success",
   });
-  const [files, setFiles] = React.useState([]);
+  const [files, setFiles] = React.useState([] as any);
 
   const handleClose = () => {
     setOpen(false);
   };
 
+  const fetchResumes = async()=>{
+    try{
+      const data = await apiget('/pdf/list-scores')
+      if(data){
+        console.log("Dta", data)
+        setSnackOpen(true);
+        setMessage({ msg: "uploaded", color: "success" })
+      }
+    }
+    catch (err: any) {
+      console.log(err);
+      setMessage({ msg: "failed", color: "error" })
+    }
+    
+  }
+
+  const fetchEmails = async(email: string, index: number)=>{
+    try{
+      const results = await apipost(`/pdf/generate-score?email=${email}` ,{})
+      if(index === (files.length -1) && results?.status === 201){
+        fetchResumes()
+      }
+    }
+    catch(err:any){
+      console.log(err);
+      setMessage({ msg: "failed", color: "error" }) 
+    }
+  }
+
   const handleSubmit = async () => {
     setOpen(false);
-    const data = { files: files };
+    const formData = new FormData();
+    files.map((file: File)=>{
+      formData.append("files", file)
+    })
     try {
-      const response = await apipost("/pdf/extract-texts", data);
-      if(response){
-        console.log(response)
-        setSnackOpen(true);
-        setMessage({msg: "uploaded", color: "success"})
-      }
-    } catch (err: any) {
-      console.log(err);
-      setMessage({msg: "failed", color: "error"})
+      const response = await axios.post(`${BASE_URL}/pdf/extract-texts`, formData, {
+        headers: {
+          "Content-Type": 'multipart/form-data'
+      }});
+    if (response) {
+      response?.data?.map((file: any, index: number)=>{
+        fetchEmails(file?.text?.email, index)
+      })
     }
-  };
+  } catch (err: any) {
+    console.log(err);
+    setMessage({ msg: "failed", color: "error" })
+  }
+};
 
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-  return (
-    <Box sx={{ height: 400, width: "100%" }}>
-      <Button variant="contained" component="label" onClick={handleClickOpen}>
-        Upload File
-      </Button>
-      <MaxWidthDialog
-        setOpen={setOpen}
-        open={open}
-        handleClose={handleClose}
-        handleSubmit={handleSubmit}
-        children={<DropzoneComponent files={files} setFiles={setFiles} />}
-      />
-      <DataGrid
-        rows={rows}
-        columns={columns}
-        slots={{
-          toolbar: GridToolbar,
-        }}
-        initialState={{
-          pagination: {
-            paginationModel: {
-              pageSize: 5,
-            },
+const handleClickOpen = () => {
+  setOpen(true);
+};
+return (
+  <Box sx={{ height: 400, width: "100%" }}>
+    <Button variant="contained" component="label" onClick={handleClickOpen}>
+      Upload File
+    </Button>
+    <MaxWidthDialog
+      setOpen={setOpen}
+      open={open}
+      handleClose={handleClose}
+      handleSubmit={handleSubmit}
+      children={<DropzoneComponent files={files} setFiles={setFiles} />}
+    />
+    <DataGrid
+      rows={rows}
+      columns={columns}
+      slots={{
+        toolbar: GridToolbar,
+      }}
+      initialState={{
+        pagination: {
+          paginationModel: {
+            pageSize: 5,
           },
-        }}
-        pageSizeOptions={[5]}
-        checkboxSelection
-        disableRowSelectionOnClick
-      />
-      <CustomSnackbar
-        open={snackOpen}
-        setOpen={setSnackOpen}
-        message={message.msg}
-        severity={message.color}
-      />
-    </Box>
-  );
+        },
+      }}
+      pageSizeOptions={[5]}
+      checkboxSelection
+      disableRowSelectionOnClick
+    />
+    <CustomSnackbar
+      open={snackOpen}
+      setOpen={setSnackOpen}
+      message={message.msg}
+      severity={message.color}
+    />
+  </Box>
+);
 }
